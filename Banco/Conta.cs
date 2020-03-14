bdc2166f540f91e693ca61fc1a5e72b8ea13d02f
prodatavinicius <52536079+prodatavinicius@users.noreply.github.com>
2020-03-13 22:06:33 -0300

    Add files via upload

---
+++ Banco/Conta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    class Conta
+    {
+        public int Numero { get; set; }
+        public double Saldo { get; private set; }
+        public string Titular { get; set; }
+
+        public void Depositar(double valor)
+        {
+            this.Saldo += valor;
+        }
+
+        public void Sacar(double valor)
+        {
+            if(valor <= Saldo)
+            this.Saldo -= valor;
+
+        }
+    }
+}
---
+++ Banco/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banco
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Conta c1;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //criando a conta
+            c1 = new Conta();
+            c1.Titular = "Maria";
+            c1.Numero = 1;
+
+            //imprimar os dados
+            textoTitular.Text = c1.Titular;
+            textoNumero.Text = Convert.ToString(c1.Numero);
+            textoSaldo.Text = Convert.ToString(c1.Saldo);
+            
+            
+        }
+
+        private void btn_Depositar_Click(object sender, EventArgs e)
+        {
+            double valor = Convert.ToDouble(ValorOperecao.Text);
+            c1.Depositar(valor);
+
+            textoSaldo.Text = Convert.ToString(c1.Saldo);
+
+
+        }
+
+        private void btn_Sacar_Click(object sender, EventArgs e)
+        {
+            double valor = Convert.ToDouble(ValorOperecao.Text);
+            c1.Sacar(valor);
+
+            textoSaldo.Text = Convert.ToString(c1.Saldo);
+
+        }
+    }
+}
